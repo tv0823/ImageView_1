6f67770243302eafe06cdc1010302744b9d5f28a
tv0823 <tv0823@bs.amalnet.k12.il>
2024-01-02 09:41:39 +0200

    new button and imageview

--- app/src/main/java/com/example/imageview_1/MainActivity.java
+++ app/src/main/java/com/example/imageview_1/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.imageview_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    ImageView img;
+    Random rd = new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        img = findViewById(R.id.img1);
+
+    }
+
+    public void pressed(View view) {
+        int num = rd.nextInt(3)+1;
+        btn.setText(""+num);
+        if (num == 1)
+            img.setImageResource(R.drawable.cat);
+        if (num == 2)
+            img.setImageResource(R.drawable.dog);
+        if (num == 3)
+            img.setImageResource(R.drawable.parrot);
     }
 }
